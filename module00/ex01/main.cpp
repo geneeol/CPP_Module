f66d86de14f55070a5551e8bcc372064ddb68261
dakang <trumpjqk@daum.net>
2023-07-01 19:48:45 +0900

    Fix: 모듈00 ex00 c++스타일 캐스팅, ex01 참고주석 추가

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -7,6 +7,9 @@ int main(void)
     PhoneBook phoneBook;
     std::string command;
 
+    // 수정하면 좋을 부분: cin은 whitespace를 기준으로 한 토큰씩 입력을 받는다.
+    // 따라서 getline을 쓰거나 cin을 사용한 이후 ignore나 getline을 통해 버퍼를
+    // 비우는 것이 바람직하다.
     while (true)
     {
         std::cout << "Enter a command: ";
