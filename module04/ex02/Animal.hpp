e5940236549403b6b70774edade468a938394faf
dakang <trumpjqk@daum.net>
2023-07-08 04:32:29 +0900

    Feat: 모듈04 ex02 완료

--- module04/ex02/Animal.hpp
+++ module04/ex02/Animal.hpp
@@ -4,16 +4,17 @@
 
 class Animal
 {
+  private:
+    Animal();
+    Animal(std::string type);
+    Animal(const Animal &other);
+
   protected:
     std::string type;
 
   public:
-    Animal();
-    Animal(std::string type);
-    Animal(const Animal &other);
     Animal &operator=(const Animal &other);
     virtual ~Animal();
-
     std::string getType() const;
     virtual void makeSound() const;
     virtual void think() const = 0;
---
+++ module04/ex02/Cat.cpp
@@ -0,0 +1,51 @@
+#include "Cat.hpp"
+#include <iostream>
+
+Cat::Cat() : Animal("defaultCat"), brain(new Brain())
+{
+    std::cout << "Cat: no argumtent constructor called" << std::endl;
+}
+
+Cat::Cat(const std::string &type) : Animal(type), brain(new Brain())
+{
+    std::cout << "Cat: string constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &other) : Animal(other), brain(new Brain(*other.brain))
+{
+    std::cout << "Cat: copy constructor called" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &other)
+{
+    std::cout << "Cat: assignment operator called" << std::endl;
+    Animal::operator=(other);
+    delete brain;
+    brain = new Brain(*other.brain);
+    return (*this);
+}
+
+Cat::~Cat()
+{
+    std::cout << "Cat: destructor called" << std::endl;
+    delete brain;
+}
+
+void Cat::makeSound() const
+{
+    std::cout << "Cat: " << type << " meow meow" << std::endl;
+}
+
+void Cat::think() const
+{
+    std::cout << "Cat: " << type << " is thinking about:" << std::endl;
+    for (int i = 0; i < brain->getSize(); i++)
+    {
+        std::cout << i << "th idea: " << brain->getIdea(i) << std::endl;
+    }
+}
+
+void Cat::addIdea(const std::string &idea)
+{
+    brain->addIdea(idea);
+}
