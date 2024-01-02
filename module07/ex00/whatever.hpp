a801ddc8b5ac399e81c70686fce7f9b7e29b8345
geneeol <trumpjqk@daum.net>
2024-01-02 20:45:09 +0900

    Feat: ex00 추가

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+    return a < b ? a : b;
+}
+
+template <typename T>
+T min(const T &a, const T &b)
+{
+    return a < b ? a : b;
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+    return a > b ? a : b;
+}
+
+template <typename T>
+T max(const T &a, const T &b)
+{
+    return a > b ? a : b;
+}
