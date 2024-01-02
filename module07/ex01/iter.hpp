b1248f466ee31cbbee5b0ca45ddaab1fa0d11f33
dakang <trumpjqk@daum.net>
2024-01-03 01:59:15 +0900

    Feat: ex01 추가

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -2,40 +2,23 @@
 
 #include <iostream>
 
-template<typename T1, typename T2>
+template <typename T1, typename T2>
 void iter(T1 *arr, size_t len, T2 f)
 {
     for (size_t i = 0; i < len; i++)
         f(arr[i]);
 }
 
-// template <typename T>
-// void iter(T *arr, int len, void (*f)(T &elem))
-// {
-//     for (int i = 0; i < len; i++)
-//         f(arr[i]);
-// }
-
-// template <typename T>
-// void iter(T *arr, int len, void (*f)(const T &elem))
-// {
-//     for (int i = 0; i < len; i++)
-//         f(arr[i]);
-// }
-
-// typedef void (*func)(int &elem);
-
 template <typename T>
-void iter(T *arr, int len, void (*f)(T elem))
+void multiply2(T &elem)
 {
-    for (int i = 0; i < len; i++)
-        f(arr[i]);
+    elem *= 2;
 }
 
 template <typename T>
-void printData(const T &elem)
+static void printData(const T &elem)
 {
-    std::cout << elem << std::endl;
+    std::cout << elem << " ";
 }
 
 // class Awesome
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,20 +1,38 @@
 #include "iter.hpp"
 
-void multiply2(int &elem)
+static void increment(int &elem)
 {
-    elem *= 2;
+    ++elem;
 }
 
 int main(void)
 {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {10, 20, 30, 40, 50};
+    float arr3[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
 
     for (size_t i = 0; i < 5; i++)
-        std::cout << arr[i] << " ";
+        std::cout << arr1[i] << " ";
     std::cout << std::endl;
-    iter(arr, 5, multiply2);
+    iter(arr1, 5, multiply2<int>);
     for (size_t i = 0; i < 5; i++)
-        std::cout << arr[i] << " ";
-    // iter(arr, 5, printData);
+        std::cout << arr1[i] << " ";
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::cout << std::endl;
+    iter(arr2, 5, printData<int>);
+    std::cout << std::endl;
+    for (size_t i = 0; i < 5; i++)
+        std::cout << arr2[i] << " ";
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    iter(arr2, 5, increment);
+    iter(arr2, 5, printData<int>);
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    iter(arr3, 5, printData<float>);
     return (0);
 }
